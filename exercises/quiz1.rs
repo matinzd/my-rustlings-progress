0e864095383b34896a068616ca1e678c662701aa
matinzd <zadehdolatabad@gmail.com>
2023-04-07 19:36:37 +0200

    mark last 3 days as done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,13 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+fn calculate_price_of_apples(count: i32) -> i32 {
+    if count > 40 {
+        return count;
+    }
 
-// Put your function here!
-// fn calculate_price_of_apples {
+    count * 2
+}
 
 // Don't modify this function!
 #[test]
