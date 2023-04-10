d07436bfde1c5cddb59719e08f22904f9fa6041d
matinzd <zadehdolatabad@gmail.com>
2023-04-10 13:43:10 +0200

    mark quiz 2

--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,7 +2,9 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+fn is_a_color_word(attempt: String) -> bool {
+    attempt == "green" || attempt == "blue" || attempt == "red"
+}
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
@@ -11,8 +13,4 @@ fn main() {
     } else {
         println!("That is not a color word I know.");
     }
-}
-
-fn is_a_color_word(attempt: &str) -> bool {
-    attempt == "green" || attempt == "blue" || attempt == "red"
 }
